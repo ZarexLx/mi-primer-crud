8568f8972d218648e91f417d8811b0684331a474
squizo <masterxgmr@gmail.com>
2023-12-23 19:06:32 -0300

    build.sh tes007

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -80,7 +80,11 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
