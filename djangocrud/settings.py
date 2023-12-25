1d07eceb0e8ff0c8f96e6ab6b5b3db21ea63b2b2
squizo <masterxgmr@gmail.com>
2023-12-25 00:28:31 -0300

    rendea

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -81,7 +81,6 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
